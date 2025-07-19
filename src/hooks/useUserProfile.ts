d51488c0ae29c0d143aab02b1450f2ec9b080fb9
Zeri-Time <blee.940604@gmail.com>
2025-07-19 16:37:10 +0900

    userProfile custom hool merge

--- src/hooks/useUserProfile.ts
+++ src/hooks/useUserProfile.ts
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { supabase } from '../lib/supabase.js'
+import { supabase } from '../lib/supabase'
 
 export interface UserProfileType {
     name: string
